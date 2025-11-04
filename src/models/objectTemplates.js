08b15958a9f248f3652e40d3c7391dfa38292fc3
Jeferson reis <adiminxtroll777@gmail.com>
2025-11-04 03:57:26 +0000

    Corrige bug de conexão e melhora autenticação

--- src/models/objectTemplates.js
+++ src/models/objectTemplates.js
@@ -6,23 +6,142 @@
 
 export default [
   {
-    tpl: "tree_oak",
-    name: "Árvore de Carvalho",
-    desc: "Uma árvore grande.",
+    tpl: 1,
+    name: "Dirt",
+    spr: 698,
+    stack: 1,
+    pickup: 0,
+    block: 1,
+    build: "-577b"
+  },
+
+
+  {
+    tpl: 2,
+    name: "Dirt",
+    spr: 698,
+    stack: 1,
+    pickup: 0,
+    block: 1,
+    build: "-576b"
+  },
+
+
+  {
+    tpl: 3,
+    name: "Dirt",
+    spr: 698,
+    stack: 1,
+    pickup: 0,
+    block: 0,
+    build: "-966"
+  },
+  {
+    tpl: 4,
+    name: "Dirt",
+    spr: 698,
+    stack: 1,
+    pickup: 0,
+    block: 0,
+    build: "-965"
+  },
+  {
+    tpl: 5,
+    name: "Dirt",
+    spr: 698,
+    stack: 1,
+    pickup: 0,
+    block: 0,
+    build: "-994"
+  },
+
+  {
+    tpl: 6,
+    name: "Dirt",
+    spr: 698,
+    stack: 1,
+    pickup: 0,
+    block: 1,
+    build: "937b"
+  },
+
+  {
+    tpl: 7,
+    name: "Sign",
+    spr: 0,
     stack: 0,
     pickup: 0,
     block: 1,
-    spr: 720,
-    build: "-305, o|-20| -289f"
   },
+
   {
-    tpl: "barrel_small",
-    name: "Barril Pequeno",
-    desc: "Um pequeno barril.",
+    tpl: 8,
+    name: "Dirt",
+    spr: 698,
+    stack: 1,
+    pickup: 0,
+    block: 0,
+    build: "-995"
+  },
+
+  {
+    tpl: 9,
+    name: "Dirt",
+    spr: 698,
+    stack: 1,
+    pickup: 0,
+    block: 1,
+    build: "-497b,-499ba"
+  },
+
+  {
+    tpl: 10,
+    name: "Spawn",
+    spr: 0,
     stack: 0,
     pickup: 0,
     block: 0,
-    spr: 812,
-    build: "812"
+  },
+
+  {
+    tpl: 11,
+    name: "Dirt",
+    spr: 698,
+    stack: 1,
+    pickup: 0,
+    block: 0,
+    build: "-979"
+  },
+
+  {
+    tpl: 12,
+    name: "Dirt",
+    spr: 698,
+    stack: 1,
+    pickup: 0,
+    block: 0,
+    build: "-961"
+  },
+
+
+  {
+    tpl: 13,
+    name: "Dirt",
+    spr: 698,
+    stack: 1,
+    pickup: 0,
+    block: 0,
+    build: "-979"
+  },
+
+
+  {
+    tpl: 14,
+    name: "Dirt",
+    spr: 698,
+    stack: 1,
+    pickup: 0,
+    block: 1,
+    build: "-340b"
   }
 ];
